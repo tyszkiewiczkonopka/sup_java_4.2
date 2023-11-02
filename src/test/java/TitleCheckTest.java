4e949bba9346167ffb5f13074bd0d79390cf9948
mtyszkiewicz <tyszkiewicz.konopqa@gmail.com>
2023-11-02 12:42:45 +0100

    Five separate tests changed from ParameterizedTests to Test with separate tags reflecting website name

--- src/test/java/TitleCheckTest.java
+++ src/test/java/TitleCheckTest.java
@@ -1,48 +1,54 @@
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TitleCheckTest extends WebDriverSetup{
-    @ParameterizedTest
+    @Test
     @Tag("regression")
     @Tag("sii.pl")
-    @ValueSource(strings = "Rozwiązania i usługi IT, inżynierii i BPO - Sii Polska")
-    void siiWebsiteTitleShouldMatchTitleFromSource(String title){
+    void siiWebsiteTitleShouldMatchTitleFromSource(){
+        String title = "Rozwiązania i usługi IT, inżynierii i BPO - Sii Polska";
+
         driver.get("https://sii.pl/");
         assertThat(driver.getTitle()).isEqualTo(title);
     }
 
-    @ParameterizedTest
+    @Test
     @Tag("regression")
     @Tag("onet.pl")
-    @ValueSource(strings = "Onet – Jesteś na bieżąco")
-    void onetWebsiteTitleShouldMatchTitleFromSource(String title){
+    void onetWebsiteTitleShouldMatchTitleFromSource(){
+        String title = "Onet – Jesteś na bieżąco";
+
         driver.get("https://www.onet.pl");
         assertThat(driver.getTitle()).isEqualTo(title);
     }
-    @ParameterizedTest
+    @Test
     @Tag("regression")
     @Tag("kotuszkowo.pl")
-    @ValueSource(strings = "Kotuszkowo- blog o kotach")
-    void kotuszkowoWebsiteTitleShouldMatchTitleFromSource(String title){
+    void kotuszkowoWebsiteTitleShouldMatchTitleFromSource(){
+        String title = "Kotuszkowo- blog o kotach";
+
         driver.get("http://kotuszkowo.pl/");
         assertThat(driver.getTitle()).isEqualTo(title);
     }
-    @ParameterizedTest
+    @Test
     @Tag("regression")
     @Tag("filmweb.pl")
-    @ValueSource(strings = "Filmweb - filmy takie jak Ty!")
-    void filmwebWebsiteTitleShouldMatchTitleFromSource(String title){
+    void filmwebWebsiteTitleShouldMatchTitleFromSource(){
+        String title = "Filmweb - filmy takie jak Ty!";
+
         driver.get("https://www.filmweb.pl/");
         assertThat(driver.getTitle()).isEqualTo(title);
     }
-    @ParameterizedTest
+    @Test
     @Tag("regression")
     @Tag("selenium.dev")
-    @ValueSource(strings = "WebDriver | Selenium")
-    void seleniumWebsiteTitleShouldMatchTitleFromSource(String title){
+    void seleniumWebsiteTitleShouldMatchTitleFromSource(){
+        String title = "WebDriver | Selenium";
+
         driver.get("https://www.selenium.dev/documentation/webdriver/");
         assertThat(driver.getTitle()).isEqualTo(title);
     }
